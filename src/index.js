42095046787a5a427ca429fa20e7516a4380c670
master_su <master_su@mail.ru>
2022-01-22 05:24:14 +0300

    Сделал ДЗ 9 async/await

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./tutorial-7/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './tutorial-8/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
